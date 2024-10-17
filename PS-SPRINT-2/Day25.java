0f07aed6b85f9527d13978ab8410854141438f90
Swayam <89599729+Swayam42@users.noreply.github.com>
2024-10-18 00:04:11 +0530

    Update Day25.java

--- PS-SPRINT-2/Day25.java
+++ PS-SPRINT-2/Day25.java
@@ -1,6 +1,14 @@
 public class Day25{
-public static void main(String[] args) {
-    
+    static int evesum(int n) {
+        int s = 0;
+        for (int i = 1; i <= n; i++) {
+            s += 2 * i;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+    System.out.println(evesum(4));
 }
 
 }
